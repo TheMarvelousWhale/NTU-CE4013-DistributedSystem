3873836c09d6dc041f8cabf20469bc079b32562b
Hoang Viet <51878941+TheMarvelousWhale@users.noreply.github.com>
2021-01-25 19:51:37 +0800

    Update RRA.java

--- src/com/company/RRA.java
+++ src/com/company/RRA.java
@@ -3,6 +3,7 @@ package com.company;
 public class RRA {
     /**
      * This class provides the constants we need to implement the Reply-Request-Acknowledge Protocol
+     * @author
      */
     public static final String ACK = "ENDMSG";
     public static final String SESSION_TERMINATE = "SESSTERM";
