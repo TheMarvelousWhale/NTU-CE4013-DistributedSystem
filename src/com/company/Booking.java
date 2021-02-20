f4c87c6a8a6488c9759cc4df7c9282377c54dffb
Benjamin Chen <benjaminchen@Benjamins-iMac.local>
2021-02-20 19:53:30 +0800

    made booking, extending, and changing in terms of 15 minutes

--- src/com/company/Booking.java
+++ src/com/company/Booking.java
@@ -16,4 +16,20 @@ public class Booking {
         this.endTime = endTime;
         this.username = username;
     }
+
+    public String getStartTime(){
+        return makeReadable(this.startTime/4) + makeReadable(15*(this.startTime%4)) + "h";
+    }
+
+    public String getEndTime(){
+        int endTime = this.endTime + 1;
+        return makeReadable(endTime/4) + makeReadable(15*(endTime%4)) + "h";
+    }
+
+    private String makeReadable(int time){
+        if (time >= 10)
+            return String.valueOf(time);
+        else
+            return "0" + time;
+    }
 }
